0dbc2ed94180c254f353556ce258d4cda4fffa5d
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 22:47:47 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,9 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each { |kid, age| puts "Happy birthday #{kid}! YOu are now #{age} years old!"}
 end
-
-
